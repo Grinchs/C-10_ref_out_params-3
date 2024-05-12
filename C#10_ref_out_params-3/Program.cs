def794b2054052329f997edaa7a9b05c57f315e4
Grinchs <marcisg2345@gmail.com>
2024-05-13 01:23:39 +0300

    Add project files.

---
+++ C#10_ref_out_params-3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_10_ref_out_params_3
+{
+    internal class Program
+    {
+        static int ParaSkaitluSumma(int[] mas, out int paraSkaitluSkaits)
+        {
+            paraSkaitluSkaits = 0;
+            int sum = 0;
+
+            foreach (int i in mas)
+            {
+                if (i % 2 == 0)
+                {
+                    paraSkaitluSkaits += i;
+                    sum++;
+                }
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadi masīva lielumu: ");
+            int liels = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int[] mas = new int[liels];
+            Random rand = new Random();
+
+            for (int i = 0; i < liels; i++)
+            {
+                mas[i] = rand.Next(1, 10);
+                Console.WriteLine(mas[i]);
+            }
+
+            int sum;
+            int paraSkaitluSkaits = ParaSkaitluSumma(mas, out sum);
+
+            Console.WriteLine("Summa: " + sum);
+            Console.WriteLine("Pāra skaitļu skaits: " + paraSkaitluSkaits);
+        }
+    }
+}
